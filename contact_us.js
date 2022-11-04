ec372ecb79d41e728f9de6547e36023ea5f4f4f4
Muhammadjon135 <sherjanovmuhammadjon@gmail.com>
2022-11-04 20:05:58 +0500

    project-enviro-chemical

---
+++ contact_us.js
@@ -0,0 +1,8 @@
+let input = document.querySelector("input");
+// let input1 = document.querySelector("input")
+// let input2 = document.querySelector("input");
+
+let succesfulSendSection = document.getElementsById("succesfully__sent__part");
+const sendBtn = () => {
+  succesfulSendSection.style.display = "block";
+};
